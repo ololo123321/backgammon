d00c26ba28f9d95abddb80e1b872a7bb5ba05a9d
ololo123321 <vitkadylin@gmail.com>
2020-12-29 20:33:28 +0300

    WIP

--- train.sh
+++ train.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-model_dir="$(pwd)/model"
-summaries_dir="$(pwd)/summaries"
-checkpoints_dir="$(pwd)/checkpoints"
+model_dir="/tmp/backgammon_agent"
 
-rm -r ${model_dir} ${summaries_dir} ${checkpoints_dir}
+rm -r ${model_dir}
 
 python main.py \
     --model_dir=${model_dir} \
-    --summaries_dir=${summaries_dir} \
-    --checkpoints_dir=${checkpoints_dir} \
-    --n_episodes=1500 \
-    --val_period=500 \
-    --n_val=100
+    --n_episodes=100000 \
+    --val_period=1000 \
+    --n_val=100 \
+    --save_period 1000 \
+    --max_to_keep 3
